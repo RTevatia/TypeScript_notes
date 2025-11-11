3c488ef1c66542c7be82ee4ad8f58ce8844a1ddf
Rahul <rtevatia2@gmail.com>
2025-11-11 17:29:33 -0500

    Add functions in Fundamentals/

--- dist/Fundamentals/builtInTypes.js
+++ dist/Fundamentals/builtInTypes.js
@@ -1,12 +1,12 @@
 "use strict";
+// let sales: number = 123_456_789;
+// let course: string = "TypeScript";
+// let is_published: boolean = true;
 Object.defineProperty(exports, "__esModule", { value: true });
-let sales = 123_456_789;
-let course = "TypeScript";
-let is_published = true;
-// Since above variables have value, 
-// type declaration is not necessary
-let sales2 = 123_456_789;
-let course2 = "TypeScript";
-let is_published2 = true;
-let level; // any type
+// // Since above variables have value, 
+// // type declaration is not necessary
+// let sales2 = 123_456_789;
+// let course2 = "TypeScript";
+// let is_published2 = true;
+// let level; // any type
 //# sourceMappingURL=builtInTypes.js.map
---
+++ src/Fundamentals/functions.ts
@@ -0,0 +1,14 @@
+// function calculateTax(income: number, taxYear?: number): number {
+//     if ((taxYear || 2022) < 2022)
+//         return income * 1.2;
+//     return income * 1.3;
+// }
+
+function calculateTax(income: number, taxYear = 2022): number {
+    if (taxYear < 2022)
+        return income * 1.2;
+    return income * 1.3;
+}
+
+console.log(calculateTax(10_000));
+console.log(calculateTax(10_000, 2021));
