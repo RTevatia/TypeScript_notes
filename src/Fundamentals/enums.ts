d616e8d435494401dcbd02d05cb4ea2cc9849f9e
Rahul <rtevatia2@gmail.com>
2025-11-11 07:45:09 -0500

    Add enums in Fundamentals/

---
+++ src/Fundamentals/enums.ts
@@ -0,0 +1,39 @@
+// const small = 1;
+// const medium = 2;
+// const large = 3;
+
+// Enums (Pascal case)
+
+// enum Size {
+//   Small,
+//   Medium,
+//   Large,
+// }
+
+// console.log(Size.Small) // By default its value will be 0
+
+// If want to explicitly assign values
+// Number can be assigned to first enum keyword,
+// rest enum keywords will have sequenced numeric values
+// assigned by ts
+
+// enum Size {
+//   Small = 1,
+//   Medium,
+//   Large,
+// }
+
+// console.log(Size.Small);
+// console.log(Size.Large);
+
+
+// Enums can also use string values but
+// each keyword must be explicitly assigned
+const enum Size {
+  Small = 'S',
+  Medium = 'M',
+  Large = 'L',
+}
+
+const size: Size = Size.Small;
+console.log(size);
