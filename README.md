81f73542e1e79464ab82a07edc8e2f7b9ea46551
Rahul <rtevatia2@gmail.com>
2025-11-10 22:43:35 -0500

    Add arrays to Fundamentals

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 # TypeScript_notes
 ## 1. Fundamentals
 - Built-in Types
-- The any Type
-- Strict Compiler Option
 - Arrays
 - Tuples
 - Enums
---
+++ src/Fundamentals/arrays.ts
@@ -0,0 +1,10 @@
+// let numbers = [1, 2, 3];
+// let numbers: number[] = [1, 2, 3];
+// let numbers: number[] = [1, 2, '3'];
+
+let numbers: number[] = [];
+numbers[0] = 1;
+numbers[1] = 10;
+// numbers[2] = 'abcd';
+
+numbers.forEach((n) => n.toFixed(2));
