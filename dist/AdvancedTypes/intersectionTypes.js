9b907c6450dd0e64c388382e121c7471f131db6d
Rahul <rtevatia2@gmail.com>
2025-11-11 21:56:37 -0500

    Add intersectionTypes.ts in AdvancedTypes/

---
+++ dist/AdvancedTypes/intersectionTypes.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let textBox = {
+    drag: () => {
+        console.log("Dragging...");
+    },
+    resize: () => {
+        console.log("Resizing...");
+    },
+};
+textBox.drag();
+textBox.resize();
+//# sourceMappingURL=intersectionTypes.js.map
