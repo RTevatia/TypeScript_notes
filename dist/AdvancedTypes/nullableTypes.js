4980fa7c4443a74268b479b1381e711541f99582
Rahul <rtevatia2@gmail.com>
2025-11-12 06:22:33 -0500

    Add nullableTypes.ts to AdvancedTypes/

---
+++ dist/AdvancedTypes/nullableTypes.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(name) {
+    if (name)
+        console.log(name.toUpperCase());
+    else
+        console.log('Hola!');
+}
+greet(null);
+//# sourceMappingURL=nullableTypes.js.map
