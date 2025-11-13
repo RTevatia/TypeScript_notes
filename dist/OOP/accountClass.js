01a410239e5123c24c6b10fdfe3a79cb4ddaa231
Rahul <rtevatia2@gmail.com>
2025-11-13 06:53:26 -0500

    Access modifier private in class Account

--- dist/OOP/accountClass.js
+++ dist/OOP/accountClass.js
@@ -3,21 +3,24 @@ Object.defineProperty(exports, "__esModule", { value: true });
 class Account {
     id;
     owner;
-    balance;
+    _balance;
     nickname; //Optional property
     constructor(id, owner, balance) {
         this.id = id;
         this.owner = owner;
-        this.balance = balance;
+        this._balance = balance;
     }
     deposit(amount) {
         if (amount <= 0)
             throw new Error("Invalid amount.");
-        this.balance += amount;
+        this._balance += amount;
+    }
+    getBalance() {
+        return this._balance;
     }
 }
 let account = new Account(1, 'Marsh', 0);
 account.deposit(100);
-console.log(account.balance);
-console.log(account instanceof Account);
+// console.log(account._balance); // not accessible as it is private
+console.log(account.getBalance());
 //# sourceMappingURL=accountClass.js.map
--- src/OOP/accountClass.ts
+++ src/OOP/accountClass.ts
@@ -1,23 +1,32 @@
 class Account {
     readonly id: number;
     owner: string;
-    balance: number;
+    private _balance: number;
     nickname?: string; //Optional property
 
     constructor(id: number, owner: string, balance: number) {
         this.id = id;
         this.owner = owner;
-        this.balance = balance;
+        this._balance = balance;
     }
 
     deposit(amount: number): void {
         if (amount <= 0)
             throw new Error("Invalid amount.");
-        this.balance += amount;
+        this._balance += amount;
     }
+
+    getBalance() {
+        return this._balance;
+    }
+
+    // Make method private
+    // private calculateTax(): number {
+    //     return this._balance *0.1 // some calculate tax logic
+    // }
 }
 
 let account = new Account(1, 'Marsh', 0);
 account.deposit(100);
-console.log(account.balance);
-console.log(account instanceof Account);
+// console.log(account._balance); // not accessible as it is private
+console.log(account.getBalance());
