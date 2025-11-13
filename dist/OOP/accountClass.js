38b7775d7399399e4b64bd86f51aad5cd4d42505
Rahul <rtevatia2@gmail.com>
2025-11-13 06:34:47 -0500

    Add accountClass.ts in OOP

---
+++ dist/OOP/accountClass.js
@@ -0,0 +1,22 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Account {
+    id;
+    owner;
+    balance;
+    constructor(id, owner, balance) {
+        this.id = id;
+        this.owner = owner;
+        this.balance = balance;
+    }
+    deposit(amount) {
+        if (amount <= 0)
+            throw new Error("Invalid amount.");
+        this.balance += amount;
+    }
+}
+let account = new Account(1, 'Marsh', 0);
+account.deposit(100);
+console.log(account.balance);
+console.log(account instanceof Account);
+//# sourceMappingURL=accountClass.js.map
