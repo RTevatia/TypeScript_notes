9399d88c4d5bec9f170c396dc5ca02efb040c04a
Rahul <rtevatia2@gmail.com>
2025-11-17 06:50:43 -0500

    docs(jsdocs): add typing and docs for calculate tax
    -Adds JSDoc annotations for imporve Type safety.
    -Enables parameter and return type checking.

--- src/Modules/tax.js
+++ src/Modules/tax.js
@@ -1,4 +1,8 @@
-// @ts-nocheck
+/**
+ * Calculates income tax.
+ * @param { number } number - Net salary after expenses.
+ * @returns { number }
+ */
 export function calculateTax(number) {
     return number * 0.3;
 }
